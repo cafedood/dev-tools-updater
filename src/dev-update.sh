9c1b7f7033acf68d04877f6fdd19ceae48314b3b
ma7rix <ztjames@gmail.com>
2016-05-24 22:43:05 -0700

    add

--- src/dev-update.sh
+++ src/dev-update.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+set -x
 
 # update git repos
-
+BASE_DIR=~/dev/code
+cd $BASE_DIR
+for i in $(find . -name ".git" | cut -c 3-); do
+  cd $i;
+  git checkout master && git pull;
+  cd $BASE_DIR;
+done
 
 # update local dependencies
-brew update && brew upgrade
-
+if type "brew" > /dev/null; then
+  brew update && brew upgrade
+fi
 
